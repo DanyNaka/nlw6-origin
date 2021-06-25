1baf2e953c332ea3a160a65767c4e6b09df973a2
DanyNaka <daninakassima@gmail.com>
2021-06-25 09:17:15 -0300

    Aula 04 - Arrow UP e Otimização do JavaScript

--- main.js
+++ main.js
@@ -20,17 +20,6 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
-
 //Testimonials carousel slider swiper
 //https://swiperjs.com/swiper-api
 const swiper = new Swiper('.swiper-container', {
@@ -57,7 +46,40 @@ scrollReveal.reveal(
    #about .image, #about .text,
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
     `,
   { interval: 100 }
 )
+
+/*--------- SCROLL ---------*/
+//mudar o header da página quando der scroll
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+//window.addEventListener('scroll', changeHeaderWhenScroll)
+
+// BACK-TO-TOP
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//quando fizer o scroll executa essas funções
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
